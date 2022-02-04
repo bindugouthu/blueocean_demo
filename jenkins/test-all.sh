4a5c18eb1198e58e593ea1100688669768828761
bindugouthu <38145939+bindugouthu@users.noreply.github.com>
2022-02-04 09:52:21 +0000

    Update test-all.sh

--- jenkins/test-all.sh
+++ jenkins/test-all.sh
@@ -1,1 +1,10 @@
 echo "testing"
+mkdir -p ./reports/test-reports/
+mkdir -p ./reports/non-test-reports/
+
+mkdir -p ./more-reports/test-reports/
+mkdir -p ./more-reports/non-test-reports/
+
+touch ./reports/test-reports/ut-1.xml
+touch ./more-reports/test-reports/ut-1.xml
+touch ./more-reports/test-reports/ut-1.txt
